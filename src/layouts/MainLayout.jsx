79434ccd6dbabbfdd3d18ddcef28e804ef9617a8
md-ahmodullah <contactme.ahmod@gmail.com>
2024-11-03 07:40:39 +0600

    Added Footer

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,9 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
 export default function MainLayout() {
   return (
-    <Navbar />
-    // Outlet
-    // Footer
+    <>
+      <Navbar />
+      <div className="min-h-[calc(100vh-300px)] py-12">
+        <Outlet />
+      </div>
+      <Footer />
+    </>
   );
 }
